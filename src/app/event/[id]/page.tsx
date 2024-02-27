eec6e9bf6cb174397662fd39610730fc0f61bb39
Alcirleia de Souza Barbosa <cissa.pmro@gmail.com>
2024-02-27 17:28:39 -0300

    Quinto commit

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -1,27 +1,18 @@
-import * as api from "@/api/site"
+import * as api from "@/api/site";
 import { Search } from "@/components/site/search";
 import { redirect } from "next/navigation";
 
-type Props = {
-    params: {
-        id: string;
-    }
-}
-export const Page = async({params}: Props) => {
-
-    //quando vem da url é uma string, mas id é number, então tem que usar o parseInt
+type Props = { params: { id: string; } }
+export const Page = async({ params }: Props) => {
     const eventItem = await api.getEvent(parseInt(params.id)); // id - 30
-    // console.log(eventItem);
-   
-    //verifica se existe o eventItem
     if( !eventItem || !eventItem.status) return redirect('/');
 
     return(
        <main className="text-center mx-auto max-w-lg p-5">
         <header>
-            <h2 className="text-2xl text-yellow-400 p-5">Amigo Secreto</h2>
-            <h1 className="text-3xl">{ eventItem.title}</h1>
-            <p className="text-sm">{eventItem.description}</p>
+            <h1 className="text-3xl mt-5 mb-2">{ eventItem.title}</h1>
+            <h2 className="text-2xl text-yellow-400">Amigo Secreto</h2>
+            <p className="text-sm mb-5">{eventItem.description}</p>
         </header>
         <Search id={eventItem.id} />
         <footer>
